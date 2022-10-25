5f55069fa4be0c3665ceb939812b7e3b9a43d5c8
Josua Jonathan Manurung <josuajonathan2014@gmail.com>
2022-10-25 12:32:47 +0700

    Update

---
+++ Percobaan/Assets/Script/Controller.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    [Header("Move Component")]
+    [SerializeField] int speed;
+
+    private Rigidbody rigidBody;
+    private Vector3 playerMovement;
+
+    void Start()
+    {
+        rigidBody = this.gameObject.GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        playerMovement = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        Move();
+    }
+
+    public void Move()
+    {
+        rigidBody.velocity = new Vector3(playerMovement.x * speed, rigidBody.velocity.y, playerMovement.z * speed);
+    }
+
+    /*public bool isFlat = true;
+    private Rigidbody rigid;
+
+    private void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Vector3 tilt = Input.acceleration;
+
+        if (isFlat)
+            tilt = Quaternion.Euler(90, 0, 0) * tilt;
+
+        rigid.AddForce(tilt);
+        Debug.DrawRay(transform.position + Vector3.up, tilt, Color.cyan);
+    }*/
+}
---
+++ Percobaan/Assets/Script/serialTest.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO.Ports;
+using UnityEngine;
+
+public class serialTest : MonoBehaviour
+{
+
+    SerialPort stream = new SerialPort("COM8", 115200);
+    public string strReceived;
+
+    public string[] strData = new string[4];
+    public string[] strData_received = new string[4];
+    public float qw, qx, qy, qz;
+
+    //public bool isFlat = true;
+    //private Rigidbody rigid;
+
+    void Start()
+    {
+        stream.Open(); //Open the Serial Stream.
+        //rigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        strReceived = stream.ReadLine(); //Read the information  
+
+        strData = strReceived.Split(',');
+        if (strData[0] != "" && strData[1] != "" && strData[2] != "" && strData[3] != "")//make sure data are reday
+        {
+            strData_received[0] = strData[0];
+            strData_received[1] = strData[1];
+            strData_received[2] = strData[2];
+            strData_received[3] = strData[3];
+
+            qw = float.Parse(strData_received[0]);
+            qx = float.Parse(strData_received[1]);
+            qy = float.Parse(strData_received[2]);
+            qz = float.Parse(strData_received[3]);
+
+            //transform.rotation = new Quaternion(-qy, -qz, qx, qw);
+        }
+
+        /*Vector3 tilt = Input.acceleration;
+        if (isFlat)
+            tilt = Quaternion.Euler(90, 0, 0) * tilt;
+
+        rigid.AddForce(tilt);
+        Debug.DrawRay(transform.position + Vector3.up, tilt);*/
+    }
+}
